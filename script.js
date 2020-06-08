81d13eca5d52de12f8cb425b45cf8fc7e1c24bba
tien-nguyen-95 <quangtiennd95@gmail.com>
2020-06-08 15:40:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+window.onload = function(){
+    let img  = document.getElementById("myImage");
+    let ul = document.getElementById("list-menu");
+    // console.log(img);
+    img.onclick = function() {
+       if(this.src.split('-')[1] == "hamburger.svg")
+        {
+            this.src = "./images/icon-close.svg";
+            ul.style.display = "flex";
+            document.getElementsByClassName("banner")[0].style.filter = "brightness(50%)";
+            document.getElementsByClassName("banner")[0].style.backgroundColor = "hsl(0, 0%, 98%)";
+            document.getElementsByClassName("banner-info")[0].style.backgroundColor = "hsl(0, 0%, 98%)";
+        }
+        else
+        {
+            this.src = "./images/icon-hamburger.svg";
+            ul.style.display = "none"
+            document.getElementsByClassName("banner")[0].style.filter = "brightness(100%)";
+            document.getElementsByClassName("banner")[0].style.backgroundColor = "white";
+            document.getElementsByClassName("banner-info")[0].style.backgroundColor = "white";
+        }
+    }
+}
